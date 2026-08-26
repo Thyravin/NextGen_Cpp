1318aad792359652d9d132d1077f9acb58634708
Thyravin <tepychi05@gmail.com>
2026-08-26 21:19:28 +0700

    homework

--- Week01_Homework_thyravin.cpp
+++ Week01_Homework_thyravin.cpp
@@ -27,52 +27,31 @@ int main() {
 
     Course course;
 
-    // Teacher information
     course.teacher.name = "Mr. Sok";
     course.teacher.age = 35;
 
-    // Point to the teacher
     course.teacherPtr = &course.teacher;
 
-    // Number of students
     course.studentCount = 3;
 
-    // Student 1
-    course.students[0].name = "Dara";
-    course.students[0].age = 20;
-    course.students[0].score = 85.5;
-
-    // Student 2
-    course.students[1].name = "Liza";
-    course.students[1].age = 21;
-    course.students[1].score = 91.0;
-
-    // Student 3
-    course.students[2].name = "Sophea";
-    course.students[2].age = 20;
-    course.students[2].score = 78.5;
-
-    // Point to each student
+    int studentCount=3;
+    Student student[studentCount] = {{"Dara", 20, 85.5}, {"Liza", 21, 91.0}, {"Sophea", 20, 78.5}};
+    for (int i = 0; i < studentCount; i++) {
+        course.students[i] = student[i];
+    }
     for (int i = 0; i < course.studentCount; i++) {
         course.studentPtr[i] = &course.students[i];
     }
-
-    // =========================
-    // Display Course Information
-    // =========================
-
     cout << "==========================================" << endl;
     cout << "            COURSE INFORMATION" << endl;
     cout << "==========================================" << endl;
 
-    // Teacher
     cout << endl;
     cout << "Teacher" << endl;
     cout << "------------------------------" << endl;
     cout << "Name : " << course.teacherPtr->name << endl;
     cout << "Age  : " << course.teacherPtr->age << endl;
 
-    // Students
     cout << endl;
     cout << "Students (" << course.studentCount << "/25)" << endl;
     cout << "------------------------------" << endl;
